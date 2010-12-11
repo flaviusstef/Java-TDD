b6805df0ed08b839c5c45775f7dc3d0b1f4a777b
Flavius Stef <flavius_stef@yahoo.com>
2010-12-11 18:38:10 +0200

    Refactored if else to switch

--- src/sis/studentinfo/RegularGradingStrategy.java
+++ src/sis/studentinfo/RegularGradingStrategy.java
@@ -4,10 +4,12 @@ public class RegularGradingStrategy implements GradingStrategy {
 
 	@Override
 	public int getGradePointsFor(Student.Grade grade) {
-		if (grade == Student.Grade.A) return 4;
-		if (grade == Student.Grade.B) return 3;
-		if (grade == Student.Grade.C) return 2;
-		if (grade == Student.Grade.D) return 1;
-		return 0;
+		switch (grade) {
+			case A: return 4;
+			case B: return 3;
+			case C: return 2;
+			case D: return 1;
+			default: return 0;
+		}
 	}
 }
